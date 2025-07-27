af051ef995c2cba3184d01bb8eb40723bc2f9022
Tyler Eftink <42481212+T-BoneMadSkrilla@users.noreply.github.com>
2025-07-27 14:04:53 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './components/CartContext';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
